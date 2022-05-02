58396453fcaa0fbe6b579b712150819efc3a8003
Peeves-Pig <1050898758@qq.com>
2022-05-02 17:06:59 +0800

    添加了Bean下的Order,添加了getset方法

--- src/main/java/Shop/Bean/Order.java
+++ src/main/java/Shop/Bean/Order.java
@@ -1,4 +1,6 @@
 package Shop.Bean;
+
+import java.util.Objects;
 /*
 *@Author: wrl
 */
@@ -8,6 +10,71 @@ public class Order {
     private String Place;
     private String Info;
     private String transaction_id;
+
+
+    public String getOid() {
+        return Oid;
+    }
+
+    public void setOid(String Oid) {
+        this.Oid = Oid;
+    }
+
+    public Double getAprice() {
+        return Aprice;
+    }
+
+    public String getPlace() {
+        return Place;
+    }
+
+    public String getInfo() {
+        return Info;
+    }
+
+    public String getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setAprice(Double aprice) {
+        Aprice = aprice;
+    }
+
+    public void setPlace(String place) {
+        Place = place;
+    }
+
+    public void setInfo(String info) {
+        Info = info;
+    }
+
+    public void setTransaction_id(String transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "Oid='" + Oid + '\'' +
+                ", Aprice=" + Aprice +
+                ", Place='" + Place + '\'' +
+                ", Info='" + Info + '\'' +
+                ", transaction_id='" + transaction_id + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(Oid, order.Oid) && Objects.equals(Aprice, order.Aprice) && Objects.equals(Place, order.Place) && Objects.equals(Info, order.Info) && Objects.equals(transaction_id, order.transaction_id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Oid, Aprice, Place, Info, transaction_id);
+    }
 }
 
 
