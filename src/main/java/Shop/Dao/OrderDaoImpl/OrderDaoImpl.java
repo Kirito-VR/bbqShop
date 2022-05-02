7f359d50509ace4eac7847eb5c029999a4b9ac4a
xm <bv87ar@163.com>
2022-05-02 17:26:08 +0800

    实现OrderDao接口类

--- src/main/java/Shop/Dao/OrderDaoImpl/OrderDaoImpl.java
+++ src/main/java/Shop/Dao/OrderDaoImpl/OrderDaoImpl.java
@@ -1,6 +1,26 @@
 package Shop.Dao.OrderDaoImpl;
 
-class OrderDaoImpl {
+import Shop.Bean.Order;
+import Shop.Dao.OrderDao;
 
+import java.util.Map;
 
+//创建订单实现
+public class OrderDaoImpl implements OrderDao {
+
+    public void updata(Order order) {
+
+    }
+
+    public void create(Order order) {
+
+    }
+
+    public void remove(Order order) {
+
+    }
+
+    public Map select(Order order) {
+        return null;
+    }
 }
