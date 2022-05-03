abd4bd8d3baa087f6fbd9a04c5430fcb23685af8
Kirito-vr <3470728937@qq.com>
2022-05-03 16:39:17 +0800

    增加测试
    Signed-off-by: Kirito-vr <3470728937@qq.com>

--- src/main/java/Shop/View/Order/orderTest.java
+++ src/main/java/Shop/View/Order/orderTest.java
@@ -4,6 +4,8 @@ import Shop.Bean.Order;
 import Shop.Dao.OrderDao;
 import Shop.Dao.OrderDaoImpl.OrderDaoImpl;
 
+import java.util.Scanner;
+
 /**
  * @Author: Cqmax
  * @Date: 2022/5/3 15:57
@@ -23,5 +25,26 @@ public class orderTest {
     public void remove(Order oid){
         OrderDao order = new OrderDaoImpl();
         order.remove(oid);
+    }
+
+    public static void main(String[] args) {
+        orderTest order = new orderTest();
+        System.out.println("请输入操作:(1.展示；2.更新；3.删除;4.退出)");
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        while(i!=5){
+        switch (i){
+            case 1:order.showOrder();
+                break;
+            case 2:order.updataOrder(new Order());
+                break;
+            case 3:order.remove(new Order());
+                break;
+            case 4:i=5;
+                break;
+            default:
+                break;
+        }
+        }
     }
 }
