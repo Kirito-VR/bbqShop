81a864fe70be90bb044c304b1df657792cd1cd23
Kirito-vr <3470728937@qq.com>
2022-05-08 11:27:40 +0800

    增加了OrderInfoServiceImpl
    Signed-off-by: Kirito-vr <3470728937@qq.com>

---
+++ src/main/java/Shop/Service/OrderInfoService.java
@@ -0,0 +1,9 @@
+package Shop.Service;
+
+/**
+ * @Author: Cqmax
+ * @Date: 2022/5/6 14:54
+ * @Version 1.0
+ */
+public interface OrderInfoService {
+}
---
+++ src/main/java/Shop/Service/OrderInfoServiceImpl/OrderInfoServiceImpl.java
@@ -0,0 +1,11 @@
+package Shop.Service.OrderInfoServiceImpl;
+
+import Shop.Service.OrderInfoService;
+
+/**
+ * @Author: Cqmax
+ * @Date: 2022/5/6 14:55
+ * @Version 1.0
+ */
+public class OrderInfoServiceImpl implements OrderInfoService {
+}
